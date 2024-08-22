1665944f686d16454c76f2e210cb0cde1a642a61
Aditi Hulawale <aditihulawale.nbnstic.it@gmail.com>
2024-08-23 00:02:22 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,22 @@
+#many value to multiple variables
+X,Y,Z = 1,2,3
+print(Z)
+
+A,B,C = "aashu dada","kalpesh","krushna"
+print(A)
+
+# one value to multiple variable
+W=K=J = "computer sci"
+print(K)
+
+#unpack collection
+stu = ["a","b","c"]
+print(stu)
+g,s,f = stu
+print(s)
+
+#data types
+P = 2j
+print(P)
+print(type(P))
+
---
+++ day3.py
@@ -0,0 +1,33 @@
+# assign string
+a="Aditi"
+b='priya'
+print(a)
+print(b)
+
+
+# multiline string
+c="""Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code
+readability with the use of significant indentation. Its language constructs and object-oriented approach
+ aim to help programmers write clear, logical code for small- and large-scale projects. 
+
+ Python is dynamically-typed and garbage-collected. It supports multiple programming paradigms, 
+ including st... Wikipedia"""
+
+
+
+#print(c)
+
+#string are arrays  array is starting for index 0
+r="Aditi Sopan Hulawale"
+print(r[6])
+
+#string length
+print(len(r))
+
+
+#slicing string
+print(r[6:10])
+print(r[:10])
+print(r[:-15])
+
+
